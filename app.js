4eb5d9aa52b1eec9551b44be6e4fe54e87749cb0
renhubgit <renik2001@gmail.com>
2022-09-04 17:23:39 +0200

    code refactoring and comments

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+// Imports
 const express = require("express")
 const messageRouter = require("./routes/messages")
-const app = express()
 
+const app = express()
+const port = 3000
+
+// Static Files
+app.use(express.static("public"))
+
+// Set Template Engine
 app.set("view engine", "ejs")
 
 app.use("/messages", messageRouter)
 
 app.get("/", (req, res) => {
-  res.render("index")
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Renato",
+      added: new Date(),
+    },
+    {
+      text: "Hello World!",
+      user: "Mark",
+      added: new Date(),
+    },
+  ]
+  res.render("index", { messages: messages })
 })
 
-app.listen(3000)
+app.listen(port)
