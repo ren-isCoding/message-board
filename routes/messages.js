ca9a9b53b715a1f5e506f12eafdcbf3dbcd50ecf
renhubgit <renik2001@gmail.com>
2022-09-06 22:46:19 +0200

    message router now renders new-message view

--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-  res.send("This is messages router")
+router.get("/new", (req, res) => {
+  res.render("new-message")
 })
 
 module.exports = router
